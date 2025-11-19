e8c92a7aadd88516e1c6dd58e1ca59e3b108584f
Lebedenko_AE <lebedenko.aleksander2015@yandex.ru>
2025-11-19 11:11:05 +0300

    Доработана функциональность по техническому заданию спринта

--- src/ru/yandex/practicum/Wordle.java
+++ src/ru/yandex/practicum/Wordle.java
@@ -1,5 +1,13 @@
 package ru.yandex.practicum;
 
+import exceptions.InvalidWordException;
+import exceptions.WordNotFoundInDictionary;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 /*
 в главном классе нам нужно:
     создать лог-файл (он должен передаваться во все классы)
@@ -12,7 +20,46 @@ package ru.yandex.practicum;
 public class Wordle {
 
     public static void main(String[] args) {
+        try (PrintWriter log = new PrintWriter(new FileWriter("log.txt"))) {
+            WordleDictionaryLoader wordleDictionaryLoader = new WordleDictionaryLoader(log);
+            WordleDictionary wordleDictionary = wordleDictionaryLoader.wordleDictionaryLoader();
+            WordleGame wordleGame = new WordleGame(wordleDictionary, log);
+            try (Scanner scanner = new Scanner(System.in)) {
+                System.out.println("Введите слово из 5 букв");
+                while (wordleGame.getSteps() < 6) {
+                    try {
+                        String word = scanner.nextLine().trim().toLowerCase();
+                        if (word.isEmpty()) {
+                            wordleGame.game(word);
+                            String newWord = wordleGame.getNewGeneratedWord();
+                            System.out.println(newWord);
+                            System.out.println(wordleGame.getMask());
+                            if (newWord.equals(wordleGame.getAnswer())) {
+                                return;
+                            }
+                            wordleGame.setSteps(wordleGame.getSteps() + 1);
+                        } else if (word.length() != 5 || !word.matches("[а-я]+")) {
+                            throw new InvalidWordException("Слово должно состоять из 5 русских букв");
+                        } else if (!wordleDictionary.getWords().contains(word)) {
+                            throw new WordNotFoundInDictionary("В словаре такого слова нет");
+                        } else {
+                            wordleGame.game(word);
+                            System.out.println(wordleGame.getMask());
+                            if (word.equals(wordleGame.getAnswer())) {
+                                return;
+                            }
+                            wordleGame.setSteps(wordleGame.getSteps() + 1);
+                        }
 
+                    } catch (InvalidWordException | WordNotFoundInDictionary e) {
+                        log.println(e.getMessage());
+                    }
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/ru/yandex/practicum/WordleDictionary.java
+++ src/ru/yandex/practicum/WordleDictionary.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum;
 
-import java.util.List;
+import java.io.PrintWriter;
+import java.util.*;
 
 /*
 этот класс содержит в себе список слов List<String>
@@ -10,5 +11,49 @@ import java.util.List;
 public class WordleDictionary {
 
     private List<String> words;
+    private final PrintWriter log;
+    Random random = new Random();
+
+    public WordleDictionary(List<String> words, PrintWriter log) {
+        this.words = newListFiveLetters(words);
+        this.log = log;
+    }
+
+    public List<String> getWords() {
+        return words;
+    }
+
+    public List<String> newListFiveLetters(List<String> list) {
+        List<String> fiveLetter = new ArrayList<>();
+        try {
+            for (String string : list) {
+                if (string.length() == 5) {
+                    fiveLetter.add(string);
+                }
+            }
+            for (int i = 0; i < fiveLetter.size(); i++) {
+                if (fiveLetter.get(i).contains("ё")) {
+                    fiveLetter.set(i, fiveLetter.get(i).replace('ё', 'е'));
+                }
+            }
+            return fiveLetter;
+        } catch (IndexOutOfBoundsException e) {
+            log.println(e.getMessage());
+        }
+        return fiveLetter;
+    }
+
+    public String generatedWord(List<String> list) {
+        try {
+            if (list == null || list.isEmpty()) {
+                throw new NullPointerException("Пустой список или равен null");
+            }
+            String word = list.get(random.nextInt(list.size()));
+            return word;
+        } catch (NullPointerException e) {
+            log.println(e.getMessage());
+        }
+        return null;
+    }
 
 }
